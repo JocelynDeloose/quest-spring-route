071ad30730baa07fdd8714322782c232802998e1
JocelynDeloose <deloosejocelyn@gmail.com>
2023-10-10 12:14:31 +0200

    final

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,34 @@
 package com.wildcodeschool.doctor.controller;
-
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+
+
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
 
-        return null;
+        if (number > 13 || number == 0) {
+
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error 404 : Impossible de récupérer l'incarnation : " + number);
+
+        } else if (number == 13) {
+
+            return new Doctor(13, "Jodie Whittaker");
+
+        } else {
+
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "303");
+        }
     }
 }
